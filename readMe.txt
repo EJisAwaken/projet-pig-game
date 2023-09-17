1ac23a359b04e7816918010944317c7a30ec0913
EJisAwaken <fenoasy@yahoo.com>
2023-09-18 01:00:13 +0300

    un jeu

---
+++ readMe.txt
@@ -0,0 +1,11 @@
+The picgame :
+
+- Ce jeux est jouable seulement par 2 joueurs ( plusieur mais c'est encore en cours de constructions) 
+
+regles et fonctionnalités : 
+
+- Le joueur un commence la partie
+- new game = pour recommencer le jeu
+- roll dice = lancer le dée ( Le joueur actif passera le dée automatiquement au joeurs suivant si il obtien le chiffre 1 ) 
+- Hold = c'est pour VALIDER tON POINT aVANT QUE TU Ailles une valeur 1
+- score du gagnant >=100 
---
+++ script.js
@@ -0,0 +1,91 @@
+let dice = document.querySelectorAll(".dice img");
+let etat = document.querySelectorAll(".etat");
+let scoreT = document.querySelectorAll(".scoreT");
+let recommencer = document.querySelector("#newGame");
+let  diceRoll= document.querySelectorAll(".imgRollDice");
+let hold = document.querySelectorAll(".imgHold");
+let currentN = document.querySelectorAll(".currentNumber");
+let dices = ["Illustration/one.png", "Illustration/two.png", "Illustration/tree.png",
+"Illustration/four.png", "Illustration/five.png", "Illustration/six.png"];
+let pointC1 =0; // Ce sont les point qui ne sont pas encore validé par le joueurs
+let pointC2 =0;
+let pointT1 =0;
+let pointT2 =0;
+
+console.log(pointC1);
+// Créé des chiffre aleatoire pour le premier joueur
+function changeDice1() {
+    let random1 = Math.floor(Math.random()*dices.length);
+        dices.forEach(function (item, index) {
+            if (random1 === index){
+                dice[0].src = item;
+                dice[0].classList.toggle("animDice");
+                setInterval(function () {
+                    dice[0].classList.remove("animDice");
+                }, 1200);
+                pointC1 = pointC1 + index;
+                currentN[0].innerHTML = pointC1+1;
+                if (random1 ===0){
+                    pointC1 = 0;
+                    currentN[0].innerHTML =pointC1;
+                }
+
+                if (pointC1 >=20){
+                    etat[0].innerHTML = "(Vous avez gagner)";
+                    etat[0].style.color = "green";
+                    etat[1].innerHTML = "(Vous avez perdu)";
+                    etat[1].style.color = "red";
+                }
+
+                recommencer.addEventListener("click", function () {
+                    pointC1 = 0;
+                    currentN[0].innerHTML =pointC1;
+                })
+
+            }
+
+        })
+}
+
+
+// Créé des chiffre aleatoire pour la deuxieme joueur
+function changeDice2() {
+    let random2 = Math.floor(Math.random()*dices.length);
+        dices.forEach(function (item, index) {
+            if (random2 === index){
+                dice[1].src = item;
+                dice[1].classList.add("animDice");
+                setInterval(function () {
+                    dice[1].classList.remove("animDice");
+                }, 1200);
+                pointC2 = pointC2 + index;
+                currentN[1].innerHTML = pointC2+1;
+                if (random2 ===0){
+                    pointC2 = 0;
+                    currentN[1].innerHTML =pointC2;
+                }
+
+                if (pointC2 >=20){
+                    etat[1].innerHTML = "(Vous avez gagner)";
+                    etat[1].style.color = "green";
+                    etat[0].innerHTML = "(Vous avez perdu)";
+                    etat[0].style.color = "red";
+                }
+
+                recommencer.addEventListener("click", function () {
+                    pointC2 = 0;
+                    currentN[1].innerHTML =pointC2;
+                })
+
+            }
+
+        })
+}
+diceRoll[0].addEventListener("click", changeDice1);
+diceRoll[1].addEventListener("click", changeDice2);
+
+
+// Fonction qui va envoyé la reponse vers total
+
+
+
